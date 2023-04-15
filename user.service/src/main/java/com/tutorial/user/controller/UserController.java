5c8af06065319a26bde47b6950850317d634078c
bryanJesus2003 <jesusbryan155@gmail.com>
2023-04-14 23:07:57 -0600

    circuitbreak

--- user.service/src/main/java/com/tutorial/user/controller/UserController.java
+++ user.service/src/main/java/com/tutorial/user/controller/UserController.java
@@ -3,14 +3,15 @@ package com.tutorial.user.controller;
 import com.tutorial.user.entity.User;
 import com.tutorial.user.models.Car;
 import com.tutorial.user.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/user")
@@ -38,6 +39,17 @@ public class UserController {
         return ResponseEntity.ok(user);
     }
 
+    @PostMapping
+    public ResponseEntity<User> save(
+            @RequestBody User user
+    ){
+        User userNew = userService.save(user);
+        return ResponseEntity.ok(userNew);
+    }
+
+    ////////////////////////////////////////////////////////////////////
+
+    @CircuitBreaker(name = "carsCB", fallbackMethod = "fallbackGetCars")
     @GetMapping("/cars/{userid}")
     public ResponseEntity<List<Car>> getCars(
             @PathVariable("userid") String userid
@@ -50,14 +62,8 @@ public class UserController {
         return ResponseEntity.ok(cars);
     }
 
-    @PostMapping
-    public ResponseEntity<User> save(
-            @RequestBody User user
-    ){
-        User userNew = userService.save(user);
-        return ResponseEntity.ok(userNew);
-    }
-
+    // save  car
+    @CircuitBreaker(name = "carsCB", fallbackMethod = "fallbackSaveCar")
     @PostMapping("/saveCar/{userId}")
     public ResponseEntity<Car> saveCar(
             @PathVariable("userId") String userId,
@@ -67,6 +73,8 @@ public class UserController {
          return ResponseEntity.ok(carNew);
     }
 
+    // Get car by id
+    @CircuitBreaker(name = "carsCB", fallbackMethod = "fallbackGetCars2")
     @GetMapping("/cars/fen/{userId}")
     public ResponseEntity<List<Car>> getCarsByUserId(
             @PathVariable("userId") String userId
@@ -79,7 +87,8 @@ public class UserController {
         return ResponseEntity.ok(cars);
     }
 
-
+    // Get all
+    @CircuitBreaker(name = "carsCB", fallbackMethod = "fallbackGetAll")
     @GetMapping("/all/{userid}")
     public ResponseEntity<Map<String, Object>> getCars(
             @PathVariable("userid") ObjectId userid
@@ -88,6 +97,29 @@ public class UserController {
         return ResponseEntity.ok(result);
     }
 
+    // Process 1
+    private ResponseEntity<List<Car>> fallbackGetCars(@PathVariable("userid") String userid, RuntimeException e){
+        return new ResponseEntity("Time out for process 1", HttpStatus.OK);
+    }
+
+    // Process 2
+    private ResponseEntity<List<Car>> fallbackSaveCar(
+            @PathVariable("userId") String userId,
+            @RequestBody Car car,
+            RuntimeException e
+    ){
+        return new ResponseEntity("Time out for process 2", HttpStatus.OK);
+    }
+
+    // Process 3
+    private ResponseEntity<List<Car>> fallbackGetCars2(@PathVariable("userId") String userId, RuntimeException e){
+        return new ResponseEntity("Time out for process 3", HttpStatus.OK);
+    }
+
+    // Process 4
+    private ResponseEntity<List<Car>> fallbackGetAll( @PathVariable("userid") ObjectId userid, RuntimeException e){
+        return new ResponseEntity("Time out for process 4", HttpStatus.OK);
+    }
 
 
 
