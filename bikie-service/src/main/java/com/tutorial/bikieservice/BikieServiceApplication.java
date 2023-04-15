812fae9a3b18c50b346fb6dd2646d949c1d9cc37
bryanJesus2003 <jesusbryan155@gmail.com>
2023-04-14 19:31:02 -0600

    config-server

---
+++ bikie-service/src/main/java/com/tutorial/bikieservice/BikieServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.bikieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikieServiceApplication.class, args);
+	}
+
+}
